4517bdbe56fd84ca466485c7ca5fbcf2eec0af36
KawauchiKentarou <77446351+KawauchiKentarou@users.noreply.github.com>
2021-12-16 10:58:15 +0900

    Delete .kadai1.mod.cmd

--- kadai1/.kadai1.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ubuntu/robotsystem_device_driver/kadai1/kadai1.mod := { echo  /home/ubuntu/robotsystem_device_driver/kadai1/kadai1.o;  echo; } > /home/ubuntu/robotsystem_device_driver/kadai1/kadai1.mod
